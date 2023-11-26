1a2b54b8aeb1458cd83603f0ab87f4ba0c00f395
William Lew <WilliamMilesLew@gmail.com>
2023-11-25 17:06:15 -0800

    Created Engine class

--- engine/src/engine.cpp
+++ engine/src/engine.cpp
@@ -1,1 +1,65 @@
 #include "engine.hpp"
+
+#define GLFW_INCLUDE_VULKAN
+#include <GLFW/glfw3.h>
+#include <vulkan/vulkan.h>
+
+static char keyDownIndex[500];
+
+void keyCallback(GLFWwindow *windowPtr, int key, int scancode, int action,
+    int mods) {
+
+  if (action == GLFW_PRESS) {
+    keyDownIndex[key] = 1;
+  }
+
+  if (action == GLFW_RELEASE) {
+    keyDownIndex[key] = 0;
+  }
+}
+
+Engine::Engine(int windowWidth, int windowHeight) :
+    windowWidth(windowWidth),
+    windowHeight(windowHeight)
+{
+  VkInstance instanceHandle = VK_NULL_HANDLE;
+  VkPhysicalDevice physicalDeviceHandle = VK_NULL_HANDLE;
+  VkDevice deviceHandle = VK_NULL_HANDLE;
+
+  VkQueue queueHandle = VK_NULL_HANDLE;
+  VkCommandPool commandPoolHandle = VK_NULL_HANDLE;
+  std::vector<VkCommandBuffer> commandBufferHandleList = {};
+
+  std::vector<DescriptorSet> descriptorSetList = {};
+  std::vector<Pipeline> pipelineList = {};
+}
+
+Engine::~Engine() {
+
+}
+
+VkResult Engine::initialize() {
+  VkResult result = VK_SUCCESS;
+
+  glfwInit();
+  glfwWindowHint(GLFW_CLIENT_API, GLFW_NO_API);
+  glfwWindowHint(GLFW_RESIZABLE, GLFW_FALSE);
+  GLFWwindow *windowPtr = glfwCreateWindow(this->windowWidth,
+      this->windowHeight, "Vulkan", NULL, NULL);
+  glfwSetInputMode(windowPtr, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+  glfwSetKeyCallback(windowPtr, keyCallback);
+
+  return result;
+}
+
+VkResult Engine::start() {
+  VkResult result = VK_SUCCESS;
+
+  return result;
+}
+
+VkResult Engine::stop() {
+  VkResult result = VK_SUCCESS;
+
+  return result;
+}
--- engine/src/engine.hpp
+++ engine/src/engine.hpp
@@ -1,8 +1,59 @@
 #pragma once
 
+#include <vulkan/vulkan.h>
+
+#include <iostream>
+#include <vector>
+
+struct DescriptorSet;
+struct Pipeline;
+
 class Engine {
 private:
+  struct DescriptorSet {
+    std::string name;
+
+    VkDescriptorPool descriptorPoolHandle;
+    VkDescriptorSetLayout descriptorSetLayoutHandle;
+  };
+
+  struct Pipeline {
+    std::string name;
+
+    VkPipelineLayout pipelineLayoutHandle;
+
+    std::vector<std::pair<VkShaderModule, VkShaderStageFlagBits>>
+        shaderModuleList;
+
+    VkPipeline pipelineHandle;
+  };
+
+  uint32_t windowWidth;
+  uint32_t windowHeight;
+
+  VkInstance instanceHandle;
+  VkPhysicalDevice physicalDeviceHandle;
+  VkDevice deviceHandle;
+
+  VkQueue queueHandle;
+  VkCommandPool commandPoolHandle;
+  std::vector<VkCommandBuffer> commandBufferHandleList;
+
+  std::vector<DescriptorSet> descriptorSetList;
+  std::vector<Pipeline> pipelineList;
 public:
-  Engine();
+  Engine(int windowWidth, int windowHeight);
   ~Engine();
+
+  VkResult initialize();
+
+  VkResult addDescriptorSet();
+  DescriptorSet *getDescriptorSet(uint32_t id);
+  DescriptorSet *getDescriptorSet(std::string name);
+
+  VkResult addPipeline(std::vector<DescriptorSet*> descriptorSetList,
+                       std::vector<std::string> shaderSourcePathList);
+
+  VkResult start();
+  VkResult stop();
 };
